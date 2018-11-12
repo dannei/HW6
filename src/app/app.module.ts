9aa1162f5182a30943d07eac22c700fa2d0bd592
Danny Le <dannylet97@gmail.com>
2018-11-11 17:45:01 -0800

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -12,9 +13,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
